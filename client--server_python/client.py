2771cbd69c52e83b68ea54c6be93525224797c26
杨艺 <596903325@qq.com>
2017-07-19 14:14:49 +0800

    Create client.py

---
+++ client--server_python/client.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 文件名：client.py
+
+import socket               # 导入 socket 模块
+
+s = socket.socket()         # 创建 socket 对象
+host = "127.0.0.1"          # 获取本地主机名
+port = 12344                # 设置端口好
+
+s.connect((host, port))
+
+while True:
+        str = raw_input("send massage:")
+        s.send(str)
+        buff = s.recv(1024)
+        print buff
+        if buff == 'server close':
+                break
+s.close()
